c540a082e2cbc8713e1116f6c5a5cbca2c618fad
Tadeáš Báča <bacatade@fit.cvut.cz>
2025-06-24 21:50:11 +0200

    Implementing symlinking

---
+++ src/file_tree.py
@@ -0,0 +1,68 @@
+from dataclasses import dataclass
+from os import curdir
+from pathlib import Path
+
+from state import State
+
+
+@dataclass
+class TreeNode:
+    path: Path
+    symlink: bool
+    children: list["TreeNode"]
+
+
+def create_tree(
+    state: State,
+    curr_dir: Path,
+    symlink_dirs: set[str],
+    in_symlink_dir: bool = False,
+) -> TreeNode:
+    should_symlink_all = in_symlink_dir or str(curr_dir) in symlink_dirs
+    could_symlink_all = should_symlink_all
+
+    children: list[TreeNode] = []
+
+    for item in curr_dir.iterdir():
+        # excluding
+        if any(x in str(item) for x in state.config.exclude):
+            could_symlink_all = False  # cannot symlink whole dir
+            continue
+
+        if item.is_file(follow_symlinks=True):  # can always symlink a simple file
+            children.append(TreeNode(item, True, []))
+
+        elif item.is_dir(follow_symlinks=True):
+            new_child = create_tree(state, item, symlink_dirs, should_symlink_all)
+            if not new_child.symlink:  # check whether we could symlink whole child
+                could_symlink_all = False
+            children.append(new_child)
+
+    relative_path = curr_dir.relative_to(state.file_dir)
+    # the corresponding dir in real home
+    corresponding_dir = Path.home() / relative_path
+
+    # check if we can really replace the whole corresponding dir
+    for item in corresponding_dir.iterdir():
+        if any(x in str(item) for x in state.config.ignore):
+            could_symlink_all = False
+            break
+
+    return TreeNode(curr_dir, could_symlink_all, children)
+
+
+def symlink(root: TreeNode, state: State) -> None:
+    # NOTE: Traversing using BFS, the graph is a directed tree, so marking visited is not needed
+    queue = [root]
+    while queue:
+        curr = queue.pop(0)
+        if curr.path.is_file() or (curr.path.is_dir() and curr.symlink):
+            State.symlink_item(
+                curr.path,
+                state.file_dir,
+                Path.home(),
+                state.get_app_data_dir(create=True) / state.BACKUP_DIR,
+            )
+        else:
+            for child in curr.children:
+                queue.append(child)
--- src/state.py
+++ src/state.py
@@ -4,13 +4,27 @@ from pathlib import Path
 import click
 
 from config import Config
+from file_tree import create_tree, symlink
 
 
 class State:
+    FORK_DIR_NAME = "forks"
+    FILE_DIR_NAME = "files"
+    BASE_BACKUP_DIR = "base_backup"
+    BACKUP_DIR = "original_files"
+
     def __init__(self, repo: Path, profile: str) -> None:
         self.repo = repo
         self.profile = profile
         self.config = Config.load(repo, profile)
+
+    @property
+    def file_dir(self) -> Path:
+        return self.repo / self.FILE_DIR_NAME
+
+    @property
+    def fork_dir(self) -> Path:
+        return self.repo / self.FORK_DIR_NAME / self.profile
 
     @classmethod
     def current(cls) -> "State":
@@ -23,6 +37,19 @@ class State:
 
         return cls(Path(repo_dir), profile)
 
+    def get_repo_data_dir(self, create=False) -> Path:
+        data_dir = self.repo / ".tdm/"
+        if create:
+            data_dir.mkdir(parents=True, exist_ok=True)
+        return data_dir
+
+    @staticmethod
+    def get_app_data_dir(create=False) -> Path:
+        app_dir = Path(click.get_app_dir("tdm"))
+        if create:
+            app_dir.mkdir(parents=True, exist_ok=True)
+        return app_dir
+
     def run_bootstrap(self) -> None:
         """Run the provided bootstrap if available"""
         if self.config.bootstrap:
@@ -31,12 +58,70 @@ class State:
                 cwd=str(self.repo / "bin"),
             )
 
-    def apply_forks(self) -> None:
+    @staticmethod
+    def symlink_item(
+        item: Path, original_base: Path, new_base: Path, backup_location: Path
+    ) -> None:
+        relative_path = item.relative_to(original_base)
+        target_path = new_base / relative_path
+        if target_path.exists():
+            if target_path.is_symlink():
+                target_path.unlink()
+            if target_path.is_file():
+                target_path.replace(backup_location / relative_path)
+        target_path.parent.mkdir(parents=True, exist_ok=True)
+        target_path.symlink_to(item, target_is_directory=item.is_dir())
+
+    def __apply_forks(self) -> None:
         """Symlink each fork to source"""
+
+        def recursively_symlink_forks(directory: Path) -> None:
+            forked_dirs = []
+            forked_dirs_file = directory / "forked_dirs"
+            if forked_dirs_file.exists():
+                with forked_dirs_file.open("r") as f:
+                    forked_dirs = f.readlines()
+
+            for item in directory.iterdir():
+                if item.name == "forked_dirs":
+                    continue
+                if item.is_file(follow_symlinks=True) or (
+                    item.is_dir()
+                    and str(item.relative_to(self.fork_dir)) in forked_dirs
+                ):
+                    self.symlink_item(
+                        item,
+                        self.fork_dir,
+                        self.file_dir,
+                        self.get_repo_data_dir(create=True) / self.BASE_BACKUP_DIR,
+                    )
+                elif item.is_dir(follow_symlinks=True):
+                    recursively_symlink_forks(item)
+
+        profile_forks = self.fork_dir
+        if not profile_forks.exists():
+            return
+
+        recursively_symlink_forks(profile_forks)
 
     def symlink(self) -> None:
         """Symlink necessary dotfiles"""
-        self.apply_forks()
+
+        self.__apply_forks()
+        repo_data_dir = self.get_repo_data_dir()
+        if not repo_data_dir.exists():
+            symlink_dirs = set()
+        else:
+            symlink_dirs_file = repo_data_dir / "symlink_dirs"
+            with symlink_dirs_file.open("r") as f:
+                symlink_dirs = set(f.readlines())
+
+        root_file_node = create_tree(
+            self,
+            self.file_dir,
+            symlink_dirs,
+        )
+        symlink(root_file_node, self)
 
     def desymlink(self) -> None:
         """Desymlink all links made by the current state"""
